5e2ab926fa910b980b2ba956827dc098570e08c3
DThgTr <121519286+DThgTr@users.noreply.github.com>
2023-10-19 16:30:02 -0500

    hsqldb setup and data population

---
+++ src/main/resources/db/hsqldb/populate.sql
@@ -0,0 +1,31 @@
+INSERT INTO customers VALUES (1, 'JeffOne', 'MontayaOne', '101 SpringCreek St.', 'CityOne', '100000001', 'mail1@corpx.com');
+INSERT INTO customers VALUES (2, 'JeffTwo', 'MontayaTwo', '102 SpringCreek St.', 'CityTwo', '100000002', 'mail2@corpx.com');
+INSERT INTO customers VALUES (3, 'JeffThree', 'MontayaThree', '103 SpringCreek St.', 'CityThree', '100000003', 'mail3@corpx.com');
+INSERT INTO customers VALUES (4, 'JeffFour', 'MontayaFour', '104 SpringCreek St.', 'CityFour', '100000004', 'mail4@corpx.com');
+INSERT INTO customers VALUES (5, 'JeffFive', 'MontayaFive', '105 SpringCreek St.', 'CityFive', '100000005', 'mail5@corpx.com');
+INSERT INTO customers VALUES (6, 'JeffSix', 'MontayaSix', '106 SpringCreek St.', 'CitySix', '100000006', 'mail6@corpx.com');
+INSERT INTO customers VALUES (7, 'JeffSeven', 'MontayaSeven', '107 SpringCreek St.', 'CitySeven', '100000007', 'mail7@corpx.com');
+INSERT INTO customers VALUES (8, 'Bob', 'Yatamon', '108 SpringCreek St.', 'CityEight', '100000008', 'mail8@corpx.com');
+INSERT INTO customers VALUES (9, 'Ted', 'Yatamon', '109 SpringCreek St.', 'CityNine', '100000009', 'mail9@corpx.com');
+INSERT INTO customers VALUES (10, 'Het', 'Yatamon', '110 SpringCreek St.', 'CityTen', '100000010', 'mail10@corpx.com');
+
+INSERT INTO fragrances VALUES (1, 'lavender');
+INSERT INTO fragrances VALUES (2, 'rose');
+INSERT INTO fragrances VALUES (3, 'jasmine');
+INSERT INTO fragrances VALUES (4, 'lemon');
+INSERT INTO fragrances VALUES (5, 'peppermint');
+INSERT INTO fragrances VALUES (6, 'Vanilla');
+
+INSERT INTO orders VALUES (1, 5, 1, 1);
+INSERT INTO orders VALUES (2, 7, 6, 2);
+INSERT INTO orders VALUES (3, 3, 2, 3);
+INSERT INTO orders VALUES (4, 4, 2, 3);
+INSERT INTO orders VALUES (5, 13, 3, 4);
+INSERT INTO orders VALUES (6, 20, 4, 5);
+INSERT INTO orders VALUES (7, 10, 1, 6);
+INSERT INTO orders VALUES (8, 11, 1, 6);
+INSERT INTO orders VALUES (9, 1, 5, 7);
+INSERT INTO orders VALUES (10, 1, 2, 8);
+INSERT INTO orders VALUES (11, 3, 5, 9);
+INSERT INTO orders VALUES (12, 6, 2, 10);
+INSERT INTO orders VALUES (13, 2, 1, 10);
---
+++ src/main/resources/db/hsqldb/setup.sql
@@ -0,0 +1,29 @@
+DROP TABLE orders IF EXISTS;
+DROP TABLE fragrances IF EXISTS;
+DROP TABLE customers IF EXISTS;
+
+CREATE TABLE fragrances (
+    id   INTEGER IDENTITY PRIMARY KEY,
+    name VARCHAR(80)
+);
+CREATE INDEX fragrances_name ON fragrances (name);
+
+CREATE TABLE customers (
+    id          INTEGER IDENTITY PRIMARY KEY,
+    first_name  VARCHAR(30),
+    last_name   VARCHAR_IGNORECASE(30),
+    address     VARCHAR(255),
+    city        VARCHAR(80),
+    telephone   VARCHAR(20),
+    email       VARCHAR(255)
+);
+CREATE INDEX customers_last_name ON customers (last_name);
+
+CREATE TABLE orders (
+    id                  INTEGER IDENTITY PRIMARY KEY,
+    quantity            INTEGER NOT NULL,
+    fragrance_type_id   INTEGER NOT NULL,
+    customer_id         INTEGER NOT NULL
+);
+ALTER TABLE orders ADD CONSTRAINT fk_customers FOREIGN KEY (customer_id) REFERENCES customers (id);
+ALTER TABLE orders ADD CONSTRAINT fk_fragrance_type FOREIGN KEY (fragrance_type_id) REFERENCES fragrances (id);
